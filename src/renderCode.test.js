ab02ff2043fcac2056be1ad18697a0a0cd153c69
Chris Richards <ripter001@gmail.com>
2021-12-11 22:57:01 -0800

    Added tests for renderCode

--- src/renderCode.test.js
+++ src/renderCode.test.js
@@ -1,4 +1,6 @@
 import { should } from 'chai';
+import sinon from 'sinon';
+
 import { renderCode } from './renderCode.js';
 
 // Setup Should style testing.
@@ -8,15 +10,28 @@ describe('renderCode', () => {
   let ctx;
   beforeEach(() => {
     ctx = {
-      fillText: () => {},
-      fillRect: () => {},
+      fillText: sinon.fake(),
+      fillRect: sinon.fake(),
     };
   });
 
   it('renders', () => {
     renderCode(ctx, 8, 8, [[{ symbol: 'N', color: '#F00' }]]);
-
-    // expect(ctx.fillStyle).toEqual('#F00');
     ctx.fillStyle.should.equal('#F00');
+  });
+
+  it('renders text from the bottom so the top line is visible.', () => {
+    renderCode(ctx, 8, 8, [[{ symbol: 'N', color: '#F00' }]]);
+    ctx.fillText.args[0][0].should.equal('N');
+    ctx.fillText.args[0][1].should.equal(0);
+    ctx.fillText.args[0][2].should.equal(8);
+  });
+
+  it('clears the canvas before rendering text.', () => {
+    renderCode(ctx, 8, 8, [[{ symbol: 'N', color: '#F00' }, null], [null, null]]);
+    ctx.fillRect.args[0][0].should.equal(0);
+    ctx.fillRect.args[0][1].should.equal(0);
+    ctx.fillRect.args[0][2].should.equal(16);
+    ctx.fillRect.args[0][3].should.equal(16);
   });
 });
