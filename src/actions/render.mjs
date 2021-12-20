bd80a5c33adffea2ef1a301cbfc3810d03bd719e
Chris Richards <ripter001@gmail.com>
2021-12-20 13:34:12 -0800

    render now takes an optional context.

--- src/actions/render.mjs
+++ src/actions/render.mjs
@@ -1,8 +1,8 @@
 import { FONT, FONT_SIZE } from '../consts/font.mjs';
 import { renderCode } from '../core/renderCode.mjs';
 
-export function render() {
+export function render(ctx = window.ctx) {
   // Render the Code to the screen.
   ctx.font = FONT;
-  renderCode(window.ctx, FONT_SIZE.WIDTH, FONT_SIZE.HEIGHT, window.code);
+  renderCode(ctx, FONT_SIZE.WIDTH, FONT_SIZE.HEIGHT, window.code);
 }
