7aef7b375e6b1ebd0e365dc351db2e5a845bb559
Chris Richards <ripter001@gmail.com>
2021-12-20 13:42:13 -0800

    Flipped argument order for inBounds so it can be bound.

--- src/utils/inBounds.mjs
+++ src/utils/inBounds.mjs
@@ -1,11 +1,11 @@
 /**
  * Returns true when the point is inside the box.
  * Box min is inclusive, Box max is exclusive.
- * @param  {[x, y]} point
  * @param  {[minX, minY, maxX, maxY]} box
+ * @param  {[x, y]} point
  * @return {Boolean}
  */
-export function inBounds(point, box) {
+export function inBounds(box, point) {
   const [x, y] = point;
   const [minX, minY, maxX, maxY] = box;
 
--- src/utils/inBounds.test.js
+++ src/utils/inBounds.test.js
@@ -4,35 +4,35 @@ import { inBounds } from './inBounds.mjs';
 describe('inBounds', () => {
   it('returns true when x is inside the box', () => {
     assert.isTrue(
-      inBounds([1, 1], [0, 0, 3, 3]),
-      'Point is inside the box'
+      inBounds([0, 0, 3, 3], [0, 2]),
+      'Point is just inside the x min and y max'
     )
   });
 
   it('return false when x < bounds', () => {
     assert.isFalse(
-      inBounds([-1, 1], [0,0, 3, 3]),
+      inBounds([0,0, 3, 3], [1, 3]),
       '-1 is outside the bounds of 0'
     );
   });
 
   it('return false when x > bounds', () => {
     assert.isFalse(
-      inBounds([3, 1], [0,0, 3, 3]),
+      inBounds([0,0, 3, 3], [1, 3]),
       '3 is outside or equal to the bounds of 3'
     );
   });
 
   it('return false when y < bounds', () => {
     assert.isFalse(
-      inBounds([1, -1], [0,0, 3, 3]),
+      inBounds([0,0, 3, 3], [1, 3]),
       '-1 is outside the bounds of 0'
     );
   });
 
   it('return false when y > bounds', () => {
     assert.isFalse(
-      inBounds([1, 3], [0,0, 3, 3]),
+      inBounds([0,0, 3, 3], [1, 3]),
       '3 is outside or equal to the bounds of 3'
     );
   });
