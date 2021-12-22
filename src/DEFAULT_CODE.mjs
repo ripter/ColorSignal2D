8b66e22c32282b6008cb2d05d5712120339658dd
Chris Richards <ripter001@gmail.com>
2021-12-21 16:44:30 -0800

    CSS updates

--- src/DEFAULT_CODE.mjs
+++ src/DEFAULT_CODE.mjs
@@ -1,10 +1,21 @@
 
+// export default [
+//   [null,          null,         null,         null, null, '*#6c642102', null],
+//   ['*#FF00FF04',  null,         null,         null, null, '*#576f7202', null],
+//   [null,          '*#FF000002', null,         null, null, '*#6c6f2002', null],
+//   [null,          null,         null,         null, null, '*#48656c02', null],
+//   [null,          null,         '*#00FF0008', '*#00FF0001', null, null, null],
+//   [null,          null,         null,         null, null, 'Ѡ#FFFFFF00', null],
+//   [null,          null,         null,         null, null, null, null],
+// ]
+
+
 export default [
   [null,          null,         null,         null, null, '*#6c642102', null],
-  ['*#FF00FF04',  null,         null,         null, null, '*#576f7202', null],
-  [null,          '*#FF000002', null,         null, null, '*#6c6f2002', null],
+  [null,          null,         null,         null, null, '*#576f7202', null],
+  [null,          null,         null,         null, null, '*#6c6f2002', null],
   [null,          null,         null,         null, null, '*#48656c02', null],
-  [null,          null,         '*#00FF0008', '*#00FF0001', null, null, null],
+  [null,          null,         null,         null, null, null,         null],
   [null,          null,         null,         null, null, 'Ѡ#FFFFFF00', null],
-  [null,          null,         null,         null, null, null, null],
+  [null,          null,         null,         null, null, null,         null],
 ]
